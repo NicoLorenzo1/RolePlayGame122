6b43ab7b0f8966a42139d6c14296edcf31f4e3ef
JuanCampos1 <88661667+JuanCampos1@users.noreply.github.com>
2022-04-28 16:32:44 -0300

    some methods added

--- src/Library/Dwarf.cs
+++ src/Library/Dwarf.cs
@@ -1,46 +1,108 @@
 using System;
+using System.Collections.Generic;
 
 namespace Library
 {
     public class Dwarf
     {
 
-        private string Name{get; }
-        private int Health{get; set; }
-        public int Vida
+        private string Name;
+        private int Health;   
+        private Item Item;
+        private int Attack;
+        private int Armor;
+        private List<Item> Inventory {get; set;  }
+        public Dwarf(string name, int health, Item item, int attack, int armor)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Item = item;
+            this.Attack = attack;
+            this.Armor = armor;
+            this.Inventory = new List<Item> { };
+        }
+
+        public string name
+        {
+            get
+            {
+                return this.Name;
+            }
+            set
+            {
+                this.Name = value;
+            }
+        }
+        public int health
         {
             get
             {
                 return this.Health;
             }
-        }   
-        //private Item Item{get; }
-        private int Damage{get; }
-        public int Daño
+            set
+            {
+                this.Health = value;
+            }
+        }
+
+        public int attack
         {
             get
             {
-                return this.Damage;
-            } 
+                return Attack;
+            }
+            set
+            {
+                Attack = value;
+            }
         }
-        private int Deffense{get; }
-        public Dwarf(string name, int health, /*Item item,*/ int damage, int deffense)
+        public int armor
         {
-            this.Name = name;
-            this.Health = health;
-            //this.Item = item;
-            this.Damage = damage;
-            this.Deffense = deffense;
-        }
-
-        public void Attack(int health)
-        {
-            health = health - this.Damage;
+            get
+            {
+                return this.Armor;
+            }
+            set
+            {
+                this.Armor = value;
+            }
         }
 
         public void recieveAttack(int damage)
         {
             this.Health = this.Health - damage;
         }
+
+        public void heal()
+        {
+            this.Health = 100;
+        }
+
+        public void EquipItem(Item item)
+        {
+            bool added = false;
+            foreach (Item value in this.Inventory)
+            {
+                if (value.ReturnName() == item.ReturnName())
+                {
+                    this.Inventory.Remove(value);
+                    this.Inventory.Add(item);
+                    this.Attack -= value.ReturnDamage();
+                    this.Armor -= value.ReturnArmor();
+                    this.Attack += item.ReturnDamage();
+                    this.Armor += item.ReturnArmor();
+                    added = true;
+                }
+            }
+
+            if (!added)
+            {
+                this.Inventory.Add(item);
+                this.Attack += item.ReturnDamage();
+                this.Armor += item.ReturnArmor();
+            }
+        }
+
+        
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,6 @@ namespace Pprogram
     {
         static void Main(string[] args)
         {
-            Dwarf dwarf = new Dwarf("Enano", 100, 20, 30);
-            Dwarf dwarf1 = new Dwarf("Enano1", 110, 40, 20);
-
-            dwarf.Attack(dwarf1.Vida);
-            Console.WriteLine(dwarf1.Vida);
-            dwarf1.recieveAttack(dwarf.Daño);
-            Console.WriteLine(dwarf1.Vida);
         }
     }
 }
