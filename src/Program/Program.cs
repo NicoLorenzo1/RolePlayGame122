6c9f7d480f4695876e760cee298e39f3bfaeffb9
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2022-04-29 01:30:22 -0300

    Cambios realizados y clases agregadas

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,35 +8,39 @@ namespace Pprogram
         static void Main(string[] args)
         {
             //Se crea el hechizo del mago y se agrega al WizardBook.
-            //Spell spell1 = new Spell("fire", "shot of fire", 40);
+            Spell spell1 = new Spell("fire", "shot of fire", 40);
             SpellBook WizardBook = new SpellBook("WizardBook");
-            //WizardBook.AddSpell(spell1);
-            
-            //Se crea el mago.
+            WizardBook.AddSpell(spell1);
+
+            //Se crea el personaje Wizzard junto a sus items y se lo equipa.
             Wizzard wizzard = new Wizzard("mago", 5, WizardBook);
-            
-            //Se crean los items.
-           // Item Sword = new Item("espada", 25, 0);
-            Item Sword2 = new Item("espada2", 25, 0);
             Item WizzardArmor = new Item("ArmorWizzard", 0, 20);
-
-            //Se equipan los items.
-            //wizzard.EquipItem(Sword);
-            //wizzard.EquipItem(Sword2);
-            //wizzard.EquipItem(Sword2);
-
-           // wizzard.EquipItem(WizzardArmor);
+            Item Sword = new Item("espada", 25, 0);
             wizzard.EquipItem(WizzardArmor);
+            wizzard.EquipItem(Sword);
 
-            //wizzard.RemoveItem(Sword);
+            //Se crea el personaje Dwarf junto a sus items y se lo equipa.
+            Dwarf dwarf = new Dwarf("dwarf", 20);
+            Item Knife = new Item("Knife", 25, 0);
+            Item dwarfArmor = new Item("dwarfArmor", 0, 20);
+            dwarf.EquipItem(Knife);
+            dwarf.EquipItem(dwarfArmor);
 
-            //wizzard.ReturnTotalAttack();
-            //SpellBook.TotalDamageBook();
-            //wizzard.cure();
-            //wizzard.ReturnTotalDefense();
+            //Se crea el personaje Human junto a sus items y se lo equipa.
+            Human human = new Human("human", 50);
+            Item weapon = new Item("weapon", 60, 0);
+            Item humanArmor = new Item("humanArmor", 0, 60);
+            human.EquipItem(weapon);
+            human.EquipItem(humanArmor);
 
-        Dwarf dwarf = new Dwarf("dwarf", 40, 20);
-        wizzard.ReceiveAttack(dwarf.TotalAttack());
+            //Se crea el personaje Elf junto a sus items y se lo equipa.
+            Elf elf = new Elf("elf", 25);
+            Item darkKnife = new Item("DarkKnife", 50, 0);
+            Item elfArmor = new Item("elfArmor", 0, 25);
+            elf.EquipItem(darkKnife);
+            elf.EquipItem(elfArmor);
+
+            wizzard.ReceiveAttack(dwarf.TotalAttack());
         }
     }
 }
